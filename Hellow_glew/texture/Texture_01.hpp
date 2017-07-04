5c81d903b3b2757c6bffe503c1a8e91a358295e7
JFChen <jfchen@JFChendeMacBook-Pro-2.local>
2017-07-04 18:33:11 +0800

    封装texture

---
+++ Hellow_glew/texture/Texture_01.hpp
@@ -0,0 +1,15 @@
+//
+//  Texture_01.hpp
+//  Hellow_glew
+//
+//  Created by JFChen on 2017/7/4.
+//  Copyright © 2017年 JFChen. All rights reserved.
+//
+
+#ifndef Texture_01_hpp
+#define Texture_01_hpp
+
+#include <stdio.h>
+
+int texture_main();
+#endif /* Texture_01_hpp */
