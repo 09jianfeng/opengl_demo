1ef0b9f7f6f67b8e04da7aa049bbbf8d31a5f837
JFChen <jfchen@JFChendeMacBook-Pro-2.local>
2017-07-04 18:28:12 +0800

    first commit

---
+++ Hellow_glew/Hellow_glew.hpp
@@ -0,0 +1,16 @@
+//
+//  Hellow_glew.hpp
+//  Hellow_glew
+//
+//  Created by JFChen on 2017/6/15.
+//  Copyright © 2017年 JFChen. All rights reserved.
+//
+
+#ifndef Hellow_glew_hpp
+#define Hellow_glew_hpp
+
+#include <stdio.h>
+
+int Hellow_glew_main();
+
+#endif /* Hellow_glew_hpp */
