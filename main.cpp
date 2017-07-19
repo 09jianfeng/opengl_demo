e63d4927a6075ea39f46ba00670013b9900a7f0d
JFChen <jfchen@JFChendeMacBook-Pro-2.local>
2017-07-19 17:57:43 +0800

    增加纹理合成

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 #include "Hellow_glew.hpp"
 #include "Hello_triangle.hpp"
 #include "Texture_01.hpp"
+#include "TextureCombine.hpp"
 
 // The MAIN function, from here we start the application and run the game loop
 int main()
 {
     //Hellow_glew_main();
 //    hello_triangle_main();
-    texture_main();
+//    texture_main();
+    combine_main();
     return 0;
 }
